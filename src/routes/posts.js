7de479d2600a0c0a98d3e633fc1af54592b5a25c
mrx7355608 <mrx7355608@gmail.com>
2024-02-16 07:27:15 +0500

    added delete and update routes for posts

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -2,6 +2,8 @@ import { Router } from "express";
 import PostModel from "../models/post.model.js";
 import isAuthenticated from "../middlewares/isAuthenticated.js";
 import postDataValidator from "../validators/posts.validator.js";
+import validator from "validator";
+import ApiError from "../utils/ApiError.js";
 
 const router = Router();
 
@@ -43,6 +45,84 @@ router.post("/", async (req, res, next) => {
         return res.status(201).json({
             ok: true,
             data: newPost,
+        });
+    } catch (err) {
+        next(err);
+    }
+});
+
+router.patch("/:id", async (req, res, next) => {
+    try {
+        const postID = req.params.id;
+
+        // Validate post id
+        if (validator.isMongoId(postID) === false) {
+            throw new ApiError("Invalid post id", 400);
+        }
+
+        // Check if post exists
+        const post = await PostModel.findById(postID);
+        if (post === null) {
+            throw new ApiError("Post not found", 404);
+        }
+
+        // Verify that post belongs to the user
+        const userID = String(req.user._id);
+        const postAuthorID = String(post.author);
+        if (userID !== postAuthorID) {
+            throw new ApiError("Only the post author can edit", 403);
+        }
+
+        // Validate new post changes
+        const newContent = req.body.content;
+        postDataValidator(newContent);
+
+        // Update the post
+        const updatedPost = await PostModel.findByIdAndUpdate(
+            postID,
+            { content: newContent },
+            { new: true }
+        );
+
+        // Return response
+        return res.status(200).json({
+            ok: true,
+            data: updatedPost,
+        });
+    } catch (err) {
+        next(err);
+    }
+});
+
+router.delete("/:id", async (req, res, next) => {
+    try {
+        const postID = req.params.id;
+
+        // Validate post id
+        if (validator.isMongoId(postID) === false) {
+            throw new ApiError("Invalid post id", 400);
+        }
+
+        // Check if post exists
+        const post = await PostModel.findById(postID);
+        if (post === null) {
+            throw new ApiError("Post not found", 404);
+        }
+
+        // Verify that post belongs to the user
+        const userID = String(req.user._id);
+        const postAuthorID = String(post.author);
+        if (userID !== postAuthorID) {
+            throw new ApiError("Only the post author can edit", 403);
+        }
+
+        // Delete the post
+        await PostModel.findByIdAndDelete(postID);
+
+        // Return response
+        return res.status(204).json({
+            ok: true,
+            data: null,
         });
     } catch (err) {
         next(err);
