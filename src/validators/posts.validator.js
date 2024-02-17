cdf2be3177ffc36ab4d4346c08a8318ccdfaac59
mrx7355608 <mrx7355608@gmail.com>
2024-02-17 05:52:55 +0500

    fixed an error message in post validation schema

--- src/validators/posts.validator.js
+++ src/validators/posts.validator.js
@@ -3,7 +3,7 @@ import ApiError from "../utils/ApiError.js";
 
 const postSchema = joi.string().required().min(2).max(1500).messages({
     "any.required": "Please enter your post content",
-    "any.empty": "An empty post cannot be created",
+    "string.empty": "An empty post cannot be created",
     "string.min": "Post should contain 2 characters at least",
     "string.max": "Post cannot be longer than 1500 characters",
     "string.base": "Invalid post content",
