0194fdb0249b44298ca1e8cf18c2b35cdfa410c3
mrx7355608 <mrx7355608@gmail.com>
2025-02-01 23:09:44 +0500

    added connection configs for mongoose in vercel

--- src/utils/db.js
+++ src/utils/db.js
@@ -2,6 +2,12 @@ const mongoose = require("mongoose");
 
 async function connectDB(url) {
     await mongoose.connect(url);
+    console.log("Connected to database");
 }
 
-module.exports = connectDB;
+async function disconnectDB() {
+    await mongoose.disconnect();
+    console.log("Disconnected from database");
+}
+
+module.exports = { connectDB, disconnectDB };
